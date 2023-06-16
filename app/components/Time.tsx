c09614773abd95be33b8919998366a0ac825fbb2
Andres Murillo <andresmurillocortazar@outlook.com>
2023-06-16 14:39:24 -0500

    Fixed H1 issues.

---
+++ app/components/Time.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Time = () => {
+    const today = new Date();
+  return (
+    <div>{today+''}</div>
+  )
+}
+
+export default Time
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Time } from './components'
 import './css/home.css'
 
 export default function Home() {
+  
   return (
+    <>
     <div className="container">
       <h1>WELCOME BACK [USER]</h1>
+      <h1><Time/></h1>
     </div>
+    </>
   )
 }
