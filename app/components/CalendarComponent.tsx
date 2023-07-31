558e47c6e640dc6ee1435f34709ac2bc34f4de09
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-31 11:47:22 -0500

    ----- Changes ---- + CSS reorganization. calendar.css will now contain all settings for CalendarComponent. + All information regarding calendar history is now on CalendarComponent.
    ---- Bugs -----

--- app/components/CalendarComponent.tsx
+++ app/components/CalendarComponent.tsx
@@ -3,13 +3,39 @@ import React, { useState } from 'react';
 import 'react-calendar/dist/Calendar.css'
 import Calendar from 'react-calendar';
 
+import './css/calendar.css'
+
 function CalendarComponent() {
   const [value, onChange] = useState(new Date()); 
 
   return (
-    <div>
-      <Calendar locale='es' onChange={()=>console.log('Funciona')} value={value} />
-    </div>
+    <>
+      <div className='calendar-container'>
+        <Calendar locale='es' onChange={()=>console.log('Funciona')} value={value} />
+      </div>
+      <div className='history'>
+        <table className='table-history'>
+          <tbody>
+            <tr>
+              <td>Citas</td>
+              <td>$50.000</td>
+            </tr>
+            <tr>
+              <td>Alimentación</td>
+              <td>$50.000</td>
+            </tr>
+            <tr>
+              <td>Entretenimiento</td>
+              <td>$50.000</td>
+            </tr>
+            <tr>
+              <td>Combustible</td>
+              <td>$50.000</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,11 @@
 import './css/home.css'
-import './css/history.css'
 import CalendarComponent from './components/CalendarComponent'
 
 export default function Home() {
   
   return (
     <div className='page-container'>
-      <div className='calendar-container'>
-        <CalendarComponent/>
-      </div>
-      <div className='history'>
-        <table className='table-history'>
-          <tbody>
-            <tr>
-              <td>Citas</td>
-              <td>$50.000</td>
-            </tr>
-            <tr>
-              <td>Alimentación</td>
-              <td>$50.000</td>
-            </tr>
-            <tr>
-              <td>Entretenimiento</td>
-              <td>$50.000</td>
-            </tr>
-            <tr>
-              <td>Combustible</td>
-              <td>$50.000</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <CalendarComponent/>
     </div>
   )
 }
