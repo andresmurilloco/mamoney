357b0b90a861e5b3fe254cc31cd0b88b61ef2b8d
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-31 11:11:23 -0500

    Time module removed from index to prevent error.

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,4 @@
 export { default as CalendarComponent } from './CalendarComponent';
 export * from './ModalTransactions';
-export { default as Time } from './Time';
 export * from './TransactionModal';
 export * from './index';
