20522df10ee9a0b8b773ca5d9e2d51bf68ab9b26
Andrés Murillo Cortázar <andresmurillocortazar@outlook.com>
2023-07-31 10:33:33 -0500

    PNPM fix

--- app/components/CalendarComponent.tsx
+++ app/components/CalendarComponent.tsx
@@ -4,9 +4,7 @@ import 'react-calendar/dist/Calendar.css'
 import Calendar from 'react-calendar';
 
 function CalendarComponent() {
-  const [value, onChange] = useState(new Date());
-  console.log(value);
-  
+  const [value, onChange] = useState(new Date()); 
 
   return (
     <div>
